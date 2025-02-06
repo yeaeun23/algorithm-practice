bfafaadff5786f9a494355a79eac039bbced9f4f
yeaeun23 <yeaeun23@naver.com>
2025-02-06 20:04:39 +0900

    알고리즘 연습

---
+++ baekjoon/누적합_10986_나머지 합.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "./baekjoon/10986.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let [n, m] = input[0].split(" ").map(Number);
+let arr = [0, ...input[1].split(" ").map(Number)];
+
+let sum = [0]; // 누적합
+
+for (let i = 1; i <= n; i++) {
+  sum.push(sum[i - 1] + arr[i]);
+}
+
+let rem = []; // 누적합을 m으로 나눈 나머지
+let counter = {}; // 나머지 개수
+
+for (let i = 0; i <= n; i++) {
+  rem.push(sum[i] % m);
+
+  if (rem[i] in counter) {
+    counter[rem[i]]++;
+  } else {
+    counter[rem[i]] = 1;
+  }
+}
+
+let res = 0;
+
+for (let i = 0; i < m; i++) {
+  if (i in counter) {
+    res += (counter[i] * (counter[i] - 1)) / 2; // nC2
+  }
+}
+
+console.log(res);
---
+++ baekjoon/누적합_11441_합 구하기.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "./baekjoon/11441.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [0, ...input[1].split(" ").map(Number)];
+let m = Number(input[2]);
+
+let prefix_sum = [0];
+
+for (let i = 1; i <= n; i++) {
+  prefix_sum.push(prefix_sum[i - 1] + arr[i]);
+}
+
+for (let i = 3; i < m + 3; i++) {
+  let [start, end] = input[i].split(" ").map(Number);
+  console.log(prefix_sum[end] - prefix_sum[start - 1]);
+}
